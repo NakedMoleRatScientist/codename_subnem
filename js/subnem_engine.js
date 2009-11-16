b23be826825b67b2dc164e8a7cab5559ee17d0ac
kiba <wikipediankiba@gmail.com>
2009-11-16 14:26:48 -0800

    don't show frame

--- js/subnem_engine.js
+++ js/subnem_engine.js
@@ -2,7 +2,7 @@ function initializeSubnemEngine(){
    var c = E.canvas(500, 500);          // create a new canvas element
    var canvas = new Canvas(c);          // create a CAKE [Canvas] for the element
    canvas.fill = [255,255,255,0.8];     // set the Canvas background to 0.8 opacity white
-   canvas.clear = false;                // show the previous frame behind the current one
+   canvas.clear = true;                 // don't show previous frame
    canvas.append(player_ship);
    document.body.appendChild(c);        // append the canvas element to document body
 }
