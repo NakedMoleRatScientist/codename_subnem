ef4a14e06b53c694d0ff0814a9530dad4f73f971
kiba <wikipediankiba@gmail.com>
2009-11-16 14:16:02 -0800

    make use of the new image

--- js/objects/player_ship.js
+++ js/objects/player_ship.js
@@ -1,7 +1,7 @@
 // This should not be in a global namespace...we should have each of these be
 // instance variables in the game world or something, rather than global-floating
 // variables. -ja
-_player_ship_image = Object.loadImage('assets/tyrian/tyrian.shp.007D3C.png');
+_player_ship_image = Object.loadImage('assets/game/fighter_x1/main.png');
 player_ship = new ImageNode(_player_ship_image);
 
 // Make the png rotate
