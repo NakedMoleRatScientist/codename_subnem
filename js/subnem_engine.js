cf6dc685adb4cc7b2fcdaab58c32980a7411f9b1
kiba <wikipediankiba@gmail.com>
2009-11-17 13:52:19 -0800

    fill black

--- js/subnem_engine.js
+++ js/subnem_engine.js
@@ -1,7 +1,7 @@
 function initializeSubnemEngine(){
    var c = E.canvas(500, 500);          // create a new canvas element
    var canvas = new Canvas(c);          // create a CAKE [Canvas] for the element
-   canvas.fill = [255,255,255,0.8];     // set the Canvas background to 0.8 opacity white
+   canvas.fill = [0,0,0,0.8];     // set the Canvas background to 0.8 opacity white
    canvas.clear = true;                 // don't show previous frame
    canvas.append(player_ship);
    document.body.appendChild(c);        // append the canvas element to document body
