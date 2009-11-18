556324d2bd25a793af80f593b2ec3fa2ad5bb505
kiba <wikipediankiba@gmail.com>
2009-11-18 09:45:07 -0800

    add images to load

--- js/objects/starfield.js
+++ js/objects/starfield.js
@@ -1,2 +1,6 @@
-_starfield_image = Object.loadImage('assets/game/starfield.png');
-starfield = new ImageNode(_starfield_image);
+_starfield_image_one = Object.loadImage('assets/game/starfield_one.png');
+_starfield_image_two = Object.loadImage('assets/game/starfield_two.png');
+_starfield_image_three = Object.loadImage('assets/game/starfield_three.png');
+starfield_one = new ImageNode(_starfield_image_one);
+starfield_two = new ImageNode(_starfield_image_two);
+starfield_three = new ImageNode(_starfield_image_three);
