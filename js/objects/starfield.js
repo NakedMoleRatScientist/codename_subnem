3437df23f24623f96697f896f84e70aa1b0772ad
kiba <wikipediankiba@gmail.com>
2009-11-18 09:47:37 -0800

    add framelistener for all of them

--- js/objects/starfield.js
+++ js/objects/starfield.js
@@ -4,3 +4,6 @@ _starfield_image_three = Object.loadImage('assets/game/starfield_three.png');
 starfield_one = new ImageNode(_starfield_image_one);
 starfield_two = new ImageNode(_starfield_image_two);
 starfield_three = new ImageNode(_starfield_image_three);
+starfield_one.addFrameListener(function(t) {});
+starfield_two.addFrameListener(function(t) {});
+starfield_three.addFrameListener(function(t) {});
