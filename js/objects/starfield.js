23203541ee6fcd15c57293865b86f2c98259e5f6
kiba <wikipediankiba@gmail.com>
2009-11-18 12:17:01 -0800

    add clones images, set position at -500, and then move to the same speed of the original

--- js/objects/starfield.js
+++ js/objects/starfield.js
@@ -2,8 +2,14 @@ _starfield_image_one = Object.loadImage('assets/game/starfield_1.png');
 _starfield_image_two = Object.loadImage('assets/game/starfield_2.png');
 _starfield_image_three = Object.loadImage('assets/game/starfield_3.png');
 starfield_one = new ImageNode(_starfield_image_one);
+clone_one = new ImageNode(_starfield_image_one);
+clone_one.x = -500
 starfield_two = new ImageNode(_starfield_image_two);
+clone_two = new ImageNode(_starfield_image_two);
+clone_two.x = -500
 starfield_three = new ImageNode(_starfield_image_three);
+clone_three = new ImageNode(_starfield_image_three);
+clone_three.x = -500
 starfield_one.addFrameListener(function(t)
 {
     this.x += 1;
@@ -13,6 +19,18 @@ starfield_two.addFrameListener(function(t)
     this.x += 2;
 });
 starfield_three.addFrameListener(function(t) 
+{
+    this.x += 3;
+});
+clone_one.addFrameListener(function(t)
+{
+    this.x += 1;
+});
+clone_two.addFrameListener(function(t)
+{
+    this.x += 2;
+});
+clone_three.addFrameListener(function(t)
 {
     this.x += 3;
 });
