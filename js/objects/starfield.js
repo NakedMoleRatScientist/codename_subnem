a0b939b4263c8c46f3cc09b673d0fdb1596b3eec
kiba <wikipediankiba@gmail.com>
2009-11-18 11:51:27 -0800

    fix naming error

--- js/objects/starfield.js
+++ js/objects/starfield.js
@@ -1,6 +1,6 @@
-_starfield_image_one = Object.loadImage('assets/game/starfield_one.png');
-_starfield_image_two = Object.loadImage('assets/game/starfield_two.png');
-_starfield_image_three = Object.loadImage('assets/game/starfield_three.png');
+_starfield_image_one = Object.loadImage('assets/game/starfield_1.png');
+_starfield_image_two = Object.loadImage('assets/game/starfield_2.png');
+_starfield_image_three = Object.loadImage('assets/game/starfield_3.png');
 starfield_one = new ImageNode(_starfield_image_one);
 starfield_two = new ImageNode(_starfield_image_two);
 starfield_three = new ImageNode(_starfield_image_three);
