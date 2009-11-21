1d9467efa4a0eea4fe3bb07df67c3a7825fac57d
kiba <wikipediankiba@gmail.com>
2009-11-20 16:32:46 -0800

    remove old code

--- js/objects/starfield.js
+++ js/objects/starfield.js
@@ -1,16 +1,3 @@
-_starfield_image_one = Object.loadImage('assets/game/starfield_1.png');
-_starfield_image_two = Object.loadImage('assets/game/starfield_2.png');
-_starfield_image_three = Object.loadImage('assets/game/starfield_3.png');
-starfield_one = new ImageNode(_starfield_image_one);
-clone_one = new ImageNode(_starfield_image_one);
-clone_one.x = -500
-starfield_two = new ImageNode(_starfield_image_two);
-clone_two = new ImageNode(_starfield_image_two);
-clone_two.x = -500
-starfield_three = new ImageNode(_starfield_image_three);
-clone_three = new ImageNode(_starfield_image_three);
-clone_three.x = -500
-
 function move(object, speed)
 {
   object.x += speed;
