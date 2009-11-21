d1ae6a968dc1ee2fab05cec520eb4cb3e4663266
kiba <wikipediankiba@gmail.com>
2009-11-20 16:05:06 -0800

    try to do mass canvas append with this function

--- js/subnem_engine.js
+++ js/subnem_engine.js
@@ -3,12 +3,6 @@ function initializeSubnemEngine(){
    var canvas = new Canvas(c);          // create a CAKE [Canvas] for the element
    canvas.fill = [0,0,0,0.8];           // set the Canvas background to 0.8 opacity black
    canvas.clear = true;                 // don't show previous frame
-   canvas.append(starfield_one);
-   canvas.append(starfield_two);
-   canvas.append(starfield_three);
-   canvas.append(clone_one);
-   canvas.append(clone_two);
-   canvas.append(clone_three);
-   canvas.append(player_ship);
+   starfield.appendLayerToCanvas(this.starfield.layer_one,canvas);
    document.body.appendChild(c);        // append the canvas element to document body
 }
