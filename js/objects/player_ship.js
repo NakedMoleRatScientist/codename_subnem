aece1d7874bccdf27f86fdcc3803b6d03920e0dd
Josh Adams <jadams@goro.(none)>
2009-11-20 23:48:45 -0600

    remove unused framelistener in player ship

--- js/objects/player_ship.js
+++ js/objects/player_ship.js
@@ -4,9 +4,6 @@
 _player_ship_image = Object.loadImage('assets/game/fighter_x1/main.png');
 player_ship = new ImageNode(_player_ship_image);
 
-// Make the png rotate
-player_ship.addFrameListener(function(t) {
-});
 function Player()
 {
 }
