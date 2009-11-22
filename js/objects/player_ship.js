b80de0d9e0969cdff1d8e6fed3f0a362e5a4159a
kiba <wikipediankiba@gmail.com>
2009-11-22 06:10:25 -0800

    make player image part of the class

--- js/objects/player_ship.js
+++ js/objects/player_ship.js
@@ -1,9 +1,10 @@
 // This should not be in a global namespace...we should have each of these be
 // instance variables in the game world or something, rather than global-floating
 // variables. -ja
-_player_ship_image = Object.loadImage('assets/game/fighter_x1/main.png');
-player_ship = new ImageNode(_player_ship_image);
 
 function Player()
 {
+    this.image = Object.loadImage('assets/game/fighter_x1/main.png');
 }
+
+player = new Player();
