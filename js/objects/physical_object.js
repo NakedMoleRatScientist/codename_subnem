374f9dcec1ba4cf6c39102daaad8876997ae536e
kiba <wikipediankiba@gmail.com>
2009-11-22 08:32:58 -0800

    add a method for converting polar coordinates into cartesian ones

--- js/objects/physical_object.js
+++ js/objects/physical_object.js
@@ -1,7 +1,15 @@
 function PhysicalObject(mass , speeed)
 {
-    this.mass = mass;
-    this.velocity = velocity;
-    this.inertia = mass * velocity;
-    this.direction = 0;
+  this.mass = mass;
+  this.velocity = velocity;
+  this.inertia = mass * velocity;
+  this.direction = 0;
+  this.radius = 1;
+  //Convert polar coordinates into retangular ones
+  covert_to_cartesian = function()
+  {
+    x = this.radius * Math.cosine(this.direction);
+    y = this.radius * Math.sine(this.direction);
+    return {x: x, y: y}
+  }
 }
