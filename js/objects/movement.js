53f83cda99da2c9543ec621c7927e21c37408e9e
kiba <wikipediankiba@gmail.com>
2009-11-24 14:00:32 -0800

    forgot a comma

--- js/objects/movement.js
+++ js/objects/movement.js
@@ -1,7 +1,7 @@
 function Movement(physical_object,image)
 {
     this.physical_object = physical_object;
-    this.image = image
+    this.image = image;
     this.move = function()
     {
       location += this.physical_object.convert_to_cartesian();
