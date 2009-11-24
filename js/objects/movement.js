58b332212597081a4c46e82c3949103f5fe73718
kiba <wikipediankiba@gmail.com>
2009-11-23 19:14:14 -0800

    add a move function

--- js/objects/movement.js
+++ js/objects/movement.js
@@ -1,4 +1,9 @@
-function Movement(physical_object)
+function Movement(physical_object,image)
 {
     this.physical_object = physical_object;
+    this.image = image
+    move = function()
+    {
+      this.image += this.physical_object.convert_to_cartesian();
+    }
 }
