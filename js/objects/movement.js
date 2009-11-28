a2f1470aa8234328ae4a56f3954be5faf74f5cb1
kiba <wikipediankiba@gmail.com>
2009-11-28 06:14:10 -0800

    an addition is not proper

--- js/objects/movement.js
+++ js/objects/movement.js
@@ -2,9 +2,9 @@ function Movement(physical_object,image)
 {
     this.physical_object = physical_object;
     this.image = image;
-    this.move = function()
+    this.act = function()
     {
-      location += this.physical_object.convert_to_cartesian();
+      location = this.physical_object.convert_to_cartesian();
       this.image.x += location.x;
       this.image.y += location.y;
     }
