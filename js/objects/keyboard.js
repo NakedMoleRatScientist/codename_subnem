1f2022234505591362365cd14e4ad5d32b9ecea0
kiba <wikipediankiba@gmail.com>
2009-11-30 08:39:28 -0800

    add a keyboard

---
+++ js/objects/keyboard.js
@@ -0,0 +1,12 @@
+Keyboard = Klass(Effect, 
+{
+    description : "Respond to player command for movement.",
+    initialize : function (player)
+    {
+	Effect.initialize.apply(this, arguments);
+	var kb_move = function (t,dt) {
+          if (this.root.keys.left)
+	      this.x -= 10; 
+        }
+    }
+}
