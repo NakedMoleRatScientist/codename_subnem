405b532a94a3f224e9a1ba2f960cc2daa4fe4f93
Josh Adams <jadams@goro.(none)>
2009-12-02 06:48:45 -0600

    made physical object a proper class

--- js/objects/physical_object.js
+++ js/objects/physical_object.js
@@ -1,58 +1,54 @@
-function PhysicalObject(mass, velocity)
-{
-  this.mass = mass;
-  this.velocity = velocity;
-  this.inertia = this.calculate_inertia;
-  this.angle = 0;
-  this.x = 0;
-  this.y = 0;
-  this.turn_step = 2;
-  this.accel_step = 0.01;
-
-  //Convert polar coordinates into retangular ones
-  this.rotate_to = function(angle){
+PhysicalObject = Klass({
+  initialize: function(mass){
+    // mass is given in kg units
+    this.mass = mass;
+    // acceleration is given in m/s**2
+    this.acceleration = 0;
+    // velocity is given in m/s
+    this.velocity = 0;
+    this.inertia = this.calculate_inertia;
+    this.angle = 0;
+    this.x = 0;
+    this.y = 0;
+    this.turn_step = 2;
+    this.accel_step = 0.01;
+  },
+  rotate_to: function(angle){
+    //Convert polar coordinates into retangular ones
     angle = (360 + angle) % 360;
     this.angle = (angle / 180) * Math.PI;
-  }
-
-  this.angle_in_degrees = function(){
+  },
+  angle_in_degrees: function(){
     var ang = (360 + this.angle) % 360;
     return (ang * 180) / Math.PI;
-  }
-
-  this.convert_to_cartesian = function()
-  {
+  },
+  convert_to_cartesian: function(){
     this.x += this.velocity * Math.cos(this.angle);
     this.y += this.velocity * Math.sin(this.angle);
-    return {x: this.x, y: this.y, angle: this.angle}
-  }
-  this.calculate_inertia = function()
-  {
-    return this.mass * this.velocity
-  }
-  this.turn_left = function()
-  {
+    return {x: this.x, y: this.y, angle: this.angle};
+  },
+  calculate_inertia: function(){
+    return this.mass * this.velocity;
+  },
+  turn_left: function(){
     this.rotate_by(-1 * this.turn_step);
-  }
-  this.turn_right = function()
-  {
+  },
+  turn_right: function(){
     this.rotate_by(this.turn_step);
-  }
-  this.rotate_by = function(amount){
+  },
+  rotate_by: function(amount){
     logger.info("angle: " + this.angle_in_degrees());
     logger.info("step: " + amount);
     this.rotate_to(this.angle_in_degrees() + amount);
-  }
-  this.accelerate = function()
-  {
+  },
+  accelerate: function(){
     this.accelerate_by(this.accel_step);
-  }
-  this.decelerate = function()
-  {
+  },
+  decelerate: function(){
     this.accelerate_by(-1 * this.accel_step);
-  }
-  this.accelerate_by = function(amount){
+  },
+  accelerate_by: function(amount){
     this.velocity += amount;
     logger.info("velocity: " + this.velocity);
   }
-}
+});
