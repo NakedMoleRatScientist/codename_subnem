c679c8d2a0f241d2804b1773f6777755aeb45479
kiba <wikipediankiba@gmail.com>
2009-12-06 10:10:24 -0800

    return motion

--- js/objects/camera.js
+++ js/objects/camera.js
@@ -6,4 +6,10 @@ Camera = Klass(
     this.width = width / 2;
     this.height = height / 2;
   }
+  return_motion: function()
+  {
+    motion_x = this.target.x - width;
+    motion_y = this.target.y - height;
+    return {x: motion_x, y: motion_y};
+  }
 });  
