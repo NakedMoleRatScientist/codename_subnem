f55efbcc4aed0100cd91c7b704e113188a3d2619
kiba <wikipediankiba@gmail.com>
2009-12-07 08:57:53 -0800

    add variable for limits and add a function for forward movement

--- js/objects/camera.js
+++ js/objects/camera.js
@@ -1,8 +1,10 @@
 Camera = Klass(
 {
-  initialize: function(target,width,length)
+  initialize: function(target,width,length,size_x,size_y)
   {
     this.target = target;
+    this.limit_x = size_x - width;
+    this.limit_y = size_y - height;
     this.width = width / 2;
     this.height = height / 2;
   }
@@ -11,5 +13,12 @@ Camera = Klass(
     motion_x = this.target.x - width;
     motion_y = this.target.y - height;
     return {x: motion_x, y: motion_y};
+  }
+  forward: function()
+  {
+    if (this.target.x > this.width)
+    {
+      width = -(this.target.x - this.width)
+    }
   }
 });  
