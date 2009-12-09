5e677cea53324410adc08c160cd2e2aad8acffd8
kiba <wikipediankiba@gmail.com>
2009-12-09 08:25:42 -0800

    create file

--- utilities/ftp.rb
+++ utilities/ftp.rb
@@ -16,11 +16,18 @@ end
 
 filename = 'utilities/config.yml'
 
-if Dir.glob(filename)
+if Dir.glob(filename) == true
 #Warning:config.yml is full of password and other information that shall not be divulged.
   config = load(filename)
 
   ftp = NET::FTP.new(config['host'],config['username'],config['password'])
   ftp.login
   ftp.close
+else
+  yaml_object = {
+    'host' => 'blah',
+    'username' => 'blah',
+    'password' => 'blah'
+  }
+  write(yaml_object, filename)
 end
