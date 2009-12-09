220267021ce196a2b62d175f4b055fe989abdfe1
kiba <wikipediankiba@gmail.com>
2009-12-09 08:33:32 -0800

    fix many bugs

--- utilities/ftp.rb
+++ utilities/ftp.rb
@@ -4,7 +4,7 @@ require 'yaml'
 
 def load_file file
   file = File.read(file)
-  file = YAML::Load(file)
+  file = YAML::load(file)
   return file
 end
 
@@ -16,18 +16,20 @@ end
 
 filename = 'utilities/config.yml'
 
-if Dir.glob(filename) == true
+if File.exist?(filename) == true
+  puts"Attempting connection"
 #Warning:config.yml is full of password and other information that shall not be divulged.
-  config = load(filename)
-
+  config = load_file(filename)
   ftp = NET::FTP.new(config['host'],config['username'],config['password'])
   ftp.login
   ftp.close
 else
+  puts"utilities/config.yml is not found. Creating file"
   yaml_object = {
     'host' => 'blah',
     'username' => 'blah',
     'password' => 'blah'
   }
   write(yaml_object, filename)
+  puts"Done. Retry utilities/ftp.rb again"
 end
