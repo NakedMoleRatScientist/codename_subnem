b0455de29f85ddd4ee45efb21dc530c5664a50bd
kiba <wikipediankiba@gmail.com>
2009-12-13 18:16:59 -0800

    add to map

--- js/subnem_engine.js
+++ js/subnem_engine.js
@@ -12,13 +12,13 @@ function initializeSubnemEngine(){
   main_scene = new Rectangle(settings.width, settings.height);
   main_scene.fill = [0, 0, 0, 1];
   main_scene.addFrameListener(stepPhysicsWorld);
+  var asteroid = new Asteroid(200,200);
   var map = new Map(30,30);
   var camera = new Camera(settings.width,settings.height);
   player_ship = new PlayerShipBox2D(350,350,camera);
-  //map.add(player_ship);
-  var asteroid = new Asteroid(200, 200);
+  map.add(player_ship);
   logger.info("adding asteroid");
-  //map.add(asteroid);
+  map.add(asteroid);
   logger.info("added asteroid");
 
   //main_scene.addFrameListener(function(t, dt){ map.move(camera.get_motion()) });
