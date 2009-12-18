42814f38234a331e0fe1a1c4b49d0623c753dc83
kiba <wikipediankiba@gmail.com>
2009-12-18 09:27:04 -0800

    just move the fricking canvas

--- js/objects/viewport.js
+++ js/objects/viewport.js
@@ -8,11 +8,8 @@ ViewPort = Klass(CanvasNode,
   },
   move: function(motion)
   {
-    for (n = 0; n < entities.length; n++)
-    {
-      entities[n].image.x += motion.offset_x;
-      entities[n].image.y += motion.offset_y;
-    }
+    this.x += motion.offset_x;
+    this.y += motion.offset_y;
   },
   appendObject: function(object)
   {
