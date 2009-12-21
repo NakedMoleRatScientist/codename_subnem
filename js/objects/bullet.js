fda75f97e56b3fed61865b674334c9eb5fecf046
kiba <wikipediankiba@gmail.com>
2009-12-21 09:19:13 -0800

    make it a canvas node

--- js/objects/bullet.js
+++ js/objects/bullet.js
@@ -1,9 +1,10 @@
-Bullet = Klass(
+Bullet = Klass(CanvasNode,
 {
   x: null,
   y: null,
   initialize: function(x_initial,y_initial)
   {
+    CanvasNode.iniitalize.call(this);
     x = x_initial;
     y = y_initial;
   }
