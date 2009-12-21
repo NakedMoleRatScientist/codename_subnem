cbd74e743e9374cf186381983cd0cbc502fb54d0
kiba <wikipediankiba@gmail.com>
2009-12-21 02:50:02 -0800

    add weapon slot

---
+++ js/objects/weapon_slot.js
@@ -0,0 +1,14 @@
+WeaponSlot = Klass(
+{
+  type: null,
+  heat_capacity: null,
+  fire_rate: null,
+  cool_rate: null,
+  _x: null,
+  _y: null,
+  initialize: function(x,y)
+  {
+    _x: x;
+    _y: y;
+  }
+}
