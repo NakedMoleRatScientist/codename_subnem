96ace0a18e4c77091f9da57f9761bff82c77bc2d
kiba <wikipediankiba@gmail.com>
2009-12-21 09:30:31 -0800

    add bullet function

--- js/objects/bullet.js
+++ js/objects/bullet.js
@@ -5,12 +5,17 @@ Bullets = Klass(CanvasNode,
   size: 3,
   direction: null,
   color: [000,170,000],
-  bullets: null,
+  bullet: null,
   initialize: function(x_initial,y_initial, dir)
   {
     CanvasNode.iniitalize.call(this);
     x = x_initial;
     y = y_initial;
     direction = dir;
+  }
+  add_bullet: function()
+  {
+    bullet = new Circle(size, { fill: color});
+    this.append(bullet);
   }
 }
