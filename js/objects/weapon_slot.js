1e23787509dfaef13cb89747995f69ea0a2f09bb
kiba <wikipediankiba@gmail.com>
2009-12-21 08:51:43 -0800

    add lot of stuff

--- js/objects/weapon_slot.js
+++ js/objects/weapon_slot.js
@@ -1,22 +1,35 @@
 WeaponSlot = Klass(
 {
   type: null,
-  heat_capacity: null,
-  parts: null,
-  fire_rate: null,
-  cool_rate: null,
+  heat_capacity: 0.0,
+  parts: [],
+  fire_rate: null, //Firerate is rounds dispened in a minute
+  heat_rate: null, //heat rate is increased per rounds fired.
+  cool_rate: null, //cool rate is measured in a minute
+  munition_capacity: null,
+  munitions_actual: null,
   _x: null,
   _y: null,
   initialize: function(x,y)
   {
     _x: x;
     _y: y;
-  }
-  mount_radiator: function(type)
+  },
+  choose: function(type)
   {
     switch(type)
     {
-      case 0: heat_capacity += 200; parts.push("radiator, 30 meters in length"); break;
+      case 0: this.weapon_rg_one();
     }
+  },
+  weapon_rg_one: function()
+  {
+    heat_capacity += 500;
+    parts.push("30mm RG| KMC inc.");
+    fire_rate = 950;
+    heat_rate = .1;
+    cool_rate = 10;
+    munition_capcity = 10000;
+    munitions_actual = 10000;
   }
 }
