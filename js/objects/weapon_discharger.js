2179cdbb832aca3035744f90c2383758de4b6934
kiba <wikipediankiba@gmail.com>
2009-12-22 09:04:59 -0800

    add discharge function

--- js/objects/weapon_discharger.js
+++ js/objects/weapon_discharger.js
@@ -9,6 +9,21 @@ WeaponDischarger = Klass(CanvasNOde, {
     CanvasNOde.initialize.call(this);
     this.x = x;
     this.y = y;
+    this._x = x;
+    this._y = y;
     this.direction = direction;
+  },
+  discharge: function(t,dt)
+  {
+    this.counter += dt;
+    this._x = this.root.mouseX;
+    this._y = this.root.mouseY;
+    if(this.counter > 5){
+      this.counter = 0;
+      this.create_bullet();
+    }
+  },
+  create_bullet: function()
+  {
   }
 }
