030c7514f970f6a71eb85ce59e270049303cdf4c
kiba <wikipediankiba@gmail.com>
2009-12-23 08:54:06 -0800

    locate it 500, 500

--- js/objects/weapon_discharger.js
+++ js/objects/weapon_discharger.js
@@ -2,20 +2,22 @@
 WeaponDischarger = Klass(CanvasNode, {
   x: null,
   y: null,
+  _x: null,
+  _y: null,
   direction: null,
   counter: null,
   initialize: function(x , y , direction)
   {
     CanvasNode.initialize.call(this);
-    this.x = 0;
-    this.y = 0;
+    this.x = 500;
+    this.y = 500;
     this._x = x;
     this._y = y;
     this.direction = direction;
   },
   create_bullet: function()
   {
-    b = new Bullet(this.x,this.y,this.direction);
+    b = new Bullet(this._x,this._y,this.direction);
     this.append(b)
   }
 });
